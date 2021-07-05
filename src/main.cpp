04f9fb89bc8daa70fc4cfb045a9f05c8ac9cd3e1
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2021-07-04 22:30:37 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,10 @@
 #include "DEFINITIONS.h"
 #define _WIN32_WINNT 0x0500
 #include <windows.h>
+#include <ctime>
 
 int main(){
+    srand(std::time(0));
     if(SHOW_CONSOLE == 0){
         ShowWindow(GetConsoleWindow(), SW_HIDE);
     }
